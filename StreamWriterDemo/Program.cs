7847971d72bd235833528145eb6dccc1e4d82812
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:17:41 +0300

    first

---
+++ StreamWriterDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var sw = new StreamWriter(@"c:\\vidobu\\days.txt"))
+            {
+
+                foreach (var day in Enum.GetNames(typeof(DayOfWeek)))
+                {
+                    sw.WriteLine(day);
+                }
+
+                sw.Flush();
+                sw.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
